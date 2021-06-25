5c2bc51913d8888d7ed709fcc2c068acb5bd0826
Kyle Kurkela <kurkela@bc.edu>
2021-06-25 16:37:08 -0400

    Added table1

--- visualize.R
+++ visualize.R
@@ -3,9 +3,25 @@
 library(tidyverse)
 library(corrr)
 library(lme4)
+library(kableExtra)
 
-col.names <- c('subject', 'pHipp', 'PREC', 'PCC', 'MPFC', 'PHC', 'RSC', 'aAG', 'pAG', 'SceErr', 'ColErr')
+col.names <- c('subject', 'pHipp', 'PREC', 'PCC', 'MPFC', 'PHC', 'RSC', 'aAG', 'pAG', 'SceCorr', 'ColCorr', 'EmoCorr')
 
 df <- read_delim('mplus/tidy_roi_data.dat', delim = ' ', col_names = col.names)
 
-df %>% select(-subject) %>% correlate() %>% fashion() %>% print()
+df %>% select(-subject) %>% correlate() %>% shave() -> corTbl
+
+corTbl %>% fashion() %>% print()
+
+# taken from Mplus
+ICCs <- c(0.037, 0.085, 0.193, 0.087, 0.124, 0.156, 0.254, 0.077, 0.208, 0.145, 0.119)
+
+df %>%
+  summarise(across(-subject, .fns = c(mean, sd, min, max))) %>%
+  pivot_longer(pHipp_1:EmoCorr_4, names_to = c('rowname', 'stat'), names_sep = '_') %>%
+  mutate(stat = factor(stat, labels = c('mean', 'sd', 'min', 'max'))) %>%
+  pivot_wider(names_from = stat, values_from = 'value') %>%
+  left_join(., corTbl, by = 'rowname') %>%
+  add_column(ICC = ICCs, .after = 'max') -> Tbl
+
+write_csv(Tbl, 'Table1.csv')
