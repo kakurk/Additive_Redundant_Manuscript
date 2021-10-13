ecf87ecc262db7054797a13b4b6a287321a9c110
kakurk <kurkela@bc.edu>
2021-10-13 14:09:20 -0400

    clean up extract roi data script

--- 01_Extract_ROI_Data.m
+++ 01_Extract_ROI_Data.m
@@ -1,94 +1,111 @@
 % Extract single trial estimates from select ROIs. Write output to a csv file for further analysis
 
+% full path to the directory this script is stored in
 root = fileparts(mfilename('fullfile'));
 
-% add spm12 to searchpath
+% add spm12 to the search path
 spm_dir = fullfile(root, 'spm12');
 addpath(spm_dir);
 
 %% Parameters
 
-roi_dir = fullfile(root, 'rois');
-roi_file = 'PM_voxel_clusters.nii'; % single .nii file. contains all ROIs for analysis
+roi_dir       = fullfile(root, 'rois');
+roi_file      = 'PM_voxel_clusters.nii';
 roi_full_file = fullfile(roi_dir, roi_file);
 
 PM_rois_names = {'pHipp' 'PREC', 'PCC', 'MPFC', 'PHC', 'RSC', 'aAG', 'pAG'};
-PM_rois_indx = cell(1,length(PM_rois_names));
+n_rois        = length(PM_rois_names);
+PM_rois_indx  = cell(1, n_rois);
 
-singleTrialEst_dir = fullfile(root, 'st_estimates');
+single_trial_est_dir = fullfile(root, 'st_estimates');
 
 %% Routine
 
 % relice ROIs
-ref = spm_select('FPList', fullfile(singleTrialEst_dir, 'sub-s001', 'spmTs'), 'Sess01_Remember_001_T.nii');
-matlabbatch{1}.spm.spatial.coreg.write.ref = {ref};
-matlabbatch{1}.spm.spatial.coreg.write.source = {roi_full_file};
+first_sub_first_st_est_dir = fullfile(single_trial_est_dir, 'sub-s001', 'spmTs');
+ref = spm_select('FPList', first_sub_first_st_est_dir, 'Sess01_Remember_001_T.nii');
+matlabbatch{1}.spm.spatial.coreg.write.ref             = {ref};
+matlabbatch{1}.spm.spatial.coreg.write.source          = {roi_full_file};
 matlabbatch{1}.spm.spatial.coreg.write.roptions.interp = 4;
-matlabbatch{1}.spm.spatial.coreg.write.roptions.wrap = [0 0 0];
-matlabbatch{1}.spm.spatial.coreg.write.roptions.mask = 0;
+matlabbatch{1}.spm.spatial.coreg.write.roptions.wrap   = [0 0 0];
+matlabbatch{1}.spm.spatial.coreg.write.roptions.mask   = 0;
 matlabbatch{1}.spm.spatial.coreg.write.roptions.prefix = 'r';
 spm_jobman('run', matlabbatch);
 
 % load ROIs
-roi_full_file = fullfile(roi_dir, ['r' roi_file]);
-ROIs_V = spm_vol(roi_full_file);
-[ROIs_Y, ROIs_XYZmm] = spm_read_vols(ROIs_V);
+resliced_roi_full_file = fullfile(roi_dir, ['r' roi_file]);
+ROIs_V                 = spm_vol(resliced_roi_full_file);
+[ROIs_Y, ROIs_XYZmm]   = spm_read_vols(ROIs_V);
 
 % specify single trial estimates
-st_estimate_files = cellstr(spm_select('FPListRec', singleTrialEst_dir, '.*Remember.*_T\.nii'));
-st_estimate_mats = cellstr(spm_select('FPListRec', singleTrialEst_dir, 'SPM.mat'));
-st_estimate_mats = st_estimate_mats(contains(st_estimate_mats, 'Remember'));
+st_estimate_files = cellstr(spm_select('FPListRec', single_trial_est_dir, '.*Remember.*_T\.nii'));
+st_estimate_mats  = cellstr(spm_select('FPListRec', single_trial_est_dir, 'SPM.mat'));
+st_estimate_mats  = st_estimate_mats(contains(st_estimate_mats, 'Remember'));
 
 assert(length(st_estimate_files) == length(st_estimate_mats), 'error')
 
-% loops
+% initalize loop variables
+n_st_estimates = length(st_estimate_files);
+subject_list   = cell(n_st_estimates, 1);
+sess_list      = cell(n_st_estimates, 1);
+trialNum_list  = cell(n_st_estimates, 1);
+ons_list       = nan(n_st_estimates, 1);
+roi_tbl        = array2table(nan(3888, n_rois), 'VariableNames', PM_rois_names);
 
-subject_list = cell(length(st_estimate_files), 1);
-sess_list = cell(length(st_estimate_files), 1);
-trialNum_list = cell(length(st_estimate_files), 1);
-ons_list = nan(length(st_estimate_files), 1);
-roi_tbl = array2table(nan(3888, length(PM_rois_names)), 'VariableNames', PM_rois_names);
+for s = 1:n_st_estimates
 
-for s = 1:length(st_estimate_files)
+   % update the user on progress
+   fprintf('\n%d/%d\n', s, n_st_estimates);
 
-   fprintf('\n%d/%d\n', s, length(st_estimate_files));
+   cur_st_estimate_file = st_estimate_files{s};
 
-   subject = regexp(st_estimate_files{s}, 'sub-s[0-9]{3}', 'match', 'once');
-   sess = regexp(st_estimate_files{s}, 'Sess[0-9]{2}', 'match', 'once');
-   trialNum = regexp(st_estimate_files{s}, '(?<=_)[0-9]{3}(?=_)', 'match', 'once');
+   % use regular expressions to extract subject, session, and trial number
+   % from the single trial estimate file name
+   subject  = regexp(cur_st_estimate_file, 'sub-s[0-9]{3}', 'match', 'once');
+   sess     = regexp(cur_st_estimate_file, 'Sess[0-9]{2}', 'match', 'once');
+   trialNum = regexp(cur_st_estimate_file, '(?<=_)[0-9]{3}(?=_)', 'match', 'once');
 
-   subF = contains(st_estimate_mats, subject);
-   sesF = contains(st_estimate_mats, sess);
-   triF = contains(st_estimate_mats, trialNum);
+   % define a set of boolean vectors to pick the correct combination of
+   % subject, session, and trial number
+   subF      = contains(st_estimate_mats, subject);
+   sesF      = contains(st_estimate_mats, sess);
+   triF      = contains(st_estimate_mats, trialNum);
    trial_mat = st_estimate_mats{subF & sesF & triF};
    assert(size(trial_mat, 1) == 1, 'error')
 
+   % load the SPM.mat file for this trial's model. Grab the trial's onset
    load(trial_mat)
    ons = SPM.Sess.U(1).ons;
 
-   for i = 1:length(PM_rois_names)
+   % extract and take the average within each ROI
+   for i = 1:n_rois
 
-      % extract ST estimate in this ROI. Take the mean. Add to the table
-      indx = find(ROIs_Y == i);
+      cur_roi = PM_rois_names{i};
+
+      % extract voxels in this ROI. Take the mean. Add to the table
+      indx    = find(ROIs_Y == i);
       [x,y,z] = ind2sub(size(ROIs_Y), indx);
-      XYZ = [x y z]';
-
-      roi_tbl{s, PM_rois_names{i}} = mean(spm_get_data(st_estimate_files{s}, XYZ), 2);
+      XYZ     = [x y z]';
+      roi_tbl{s, cur_roi} = mean(spm_get_data(cur_st_estimate_file, XYZ), 2);
 
    end
 
-  % store everything
-  subject_list{s} = subject;
-  sess_list{s} = sess;
+  % store everything in the appropriate vector
+  subject_list{s}  = subject;
+  sess_list{s}     = sess;
   trialNum_list{s} = trialNum;
-  ons_list(s) = ons;
+  ons_list(s)      = ons;
 
   clc;
 
 end
 
-metaData_tbl = table(subject_list, sess_list, trialNum_list, ons_list, 'VariableName', {'subject', 'sess', 'trialNum', 'ons'});
+% create a table of the meta data
+metaData_tbl = table(subject_list, sess_list, trialNum_list, ons_list, ...
+                     'VariableName', {'subject', 'sess', 'trialNum', 'ons'});
 
+% concatenate columns to create final table
 final_tbl = [metaData_tbl, roi_tbl];
 
-writetable(final_tbl, 'Extracted_ROI_data.csv', 'Filetype', 'text');
+% write
+writetable(final_tbl, 'intermediate/01_Extracted_ROI_data.csv', 'Filetype', 'text');
