a0662cd9bd372c655dfb0c48f218bea58135dab4
kakurk <kurkela@bc.edu>
2021-10-13 16:40:14 -0400

    updates

--- 02_tidy.R
+++ 02_tidy.R
@@ -4,108 +4,120 @@
 
 shhh <- suppressPackageStartupMessages
 
-shhh(require(tidyverse))
-shhh(require(pbapply))
-shhh(require(tictoc))
-shhh(require(glue))
-shhh(require(tictoc))
-shhh(require(assertthat))
-shhh(require(magrittr))
+shhh(library(tidyverse))
+shhh(library(pbapply))
+shhh(library(tictoc))
+shhh(library(glue))
+shhh(library(tictoc))
+shhh(library(assertthat))
+shhh(library(magrittr))
 
-nest   <- nest_legacy
-unnest <- unnest_legacy
-
-# parameters --------------------------------------------------------------
-# user specified input parameters
-
-root <- '/gsfs0/data/kurkela/Desktop/Additive_Redundant_Manuscript'
-
-# Where the extracted ROI data is
-extracted.data.File <- file.path(root, 'Extracted_ROI_data.csv')
-
-# load data ---------------------------------------------------------------
-
-cat('Loading Data:\n')
-
-# load extracted ROI data from the csv files
-
-# body --------------------------------------------------------------------
-
-betas.df <- read_csv(extracted.data.File) %>%
-            mutate(across('subject', factor))
-
-# clean -------------------------------------------------------------------
-
-# The orbit data directory on sirius
-orbit_data.dir <- file.path(root, 'orbit-data')
-
-# the subject exclusions csv
-SS_exclusions    <- file.path(orbit_data.dir, 'derivs', 'excluded-runs-elife.csv')
-SelectCols       <- cols_only(SubID = col_character(), Memory = col_logical())
-SS_exclusions.df <- read_csv(SS_exclusions, col_types = SelectCols)
-
-# the fmri behavioral data
-behav_data <- file.path(orbit_data.dir, 'behavior', 'AllData_OrbitfMRI-behavior.csv')
-behav.df   <- read_csv(file = behav_data) %>%
-              mutate(Run = as.integer(Run))
-
-BIDS_format_ID <- function(NumericID, BIDS.prepend = 's'){
+numeric_to_bids <- function(NumericID, BIDS.prepend = 's'){
   # Converts a numeric subject ID to a BIDS formatted one.
   # Zero pads the numeric ID and place 'sub-s' in front
   # NumericID can be a character or a numeric.
-
+  
   Zero.Pad.ID <- str_pad(NumericID, width = 3, side = 'left', pad = '0')
   BIDS.SubID  <- str_c(BIDS.prepend, Zero.Pad.ID)
-
+  
 }
 
 calc_quality <- function(x, thresh){
   # calculate "Quality" by excluding high error trials above a 
   # predefined threshold and scaling the remaining low error
   # trials on a scale of 0 - 1
+
   thresholded <- ifelse(x > thresh, NA, x)
   thresholded <- 1 - thresholded / max(thresholded, na.rm = T)
   if_else(is.na(thresholded), 0, thresholded)
+
 }
 
-# We are going to tidy the behav.df to match that of the betas.df
-behav.df %<>%
-  # Changes the numeric subject ID system of `behav.df`
-  # to the BIDS format of `betas.df`
-  mutate(SubID = map_chr(SubID, BIDS_format_ID, BIDS.prepend = 'sub-s')) %>%
-  mutate(Run = if_else(SubID == 'sub-s009', as.integer(Run - 1), Run)) %>%
-  mutate(onsetRemember = round(onsetRemember, 3)) %>%
-  # 57 and 30 are from Cooper & Ritchey (2019)
+# parameters --------------------------------------------------------------
+
+# the project directory
+root <- '~/Desktop/Additive_Redundant_Manuscript/'
+
+# Where the extracted ROI data is
+extracted_data_file <- file.path(root, 'intermediate', '01_Extracted_ROI_data.csv')
+
+# The orbit data directory
+orbit_data_dir <- file.path(root, 'orbit-data')
+
+# load data ---------------------------------------------------------------
+
+cat('Loading Data:\n')
+
+betas_df <- read_csv(extracted_data_file) %>%
+            mutate(across('subject', factor))
+
+# clean -------------------------------------------------------------------
+
+# the subject exclusions csv
+SS_exclusions    <- file.path(orbit_data_dir, 'derivs', 'excluded-runs-elife.csv')
+select_cols      <- cols_only(SubID = col_character(), Memory = col_logical())
+SS_exclusions_df <- read_csv(SS_exclusions, col_types = select_cols)
+
+# the fmri behavioral data
+behav_file <- file.path(orbit_data_dir, 'behavior', 'AllData_OrbitfMRI-behavior.csv')
+behav_df   <- read_csv(file = behav_file) %>%
+              mutate(Run = as.integer(Run))
+
+# Tidy the behav_df to match betas_df
+
+# Changes the numeric subject ID system of `behav_df`
+# to BIDS format. Example: 01 -> sub-s001
+behav_df %>%
+  mutate(SubID = map_chr(SubID, numeric_to_bids, BIDS.prepend = 'sub-s')) -> behav_df
+
+# Subject sub-s009 is missing the first session. The Run column of the behavioral data 
+# (`behav_df`) has Runs 2-6 for subject sub-s009. The session column of the betas data
+# frame (`betas_df`) has sessions 1-5 for subejct sub-s009. By subtracting 1 from the
+# Run column just for subject sub-s009, these columns match
+behav_df %>%
+  mutate(Run = if_else(SubID == 'sub-s009', as.integer(Run - 1), Run)) -> behav_df
+
+# Round onsetRemember to 3 decimal places. This allows for better matching of the
+# behav_df and the betas_df using dplyr::left_join() below
+behav_df %>%
+mutate(onsetRemember = round(onsetRemember, 3)) -> behav_df
+  
+# Using the calc_quality function defined above, calculate memory quality for each feature
+# The 57 and 30 cutoffs are from Cooper & Ritchey (2019)
+behav_df %>%
   mutate(ColQuality = calc_quality(ColAbsError, thresh = 57)) %>%
   mutate(SceQuality = calc_quality(SceAbsError, thresh = 30)) %>%
-  mutate(MemoryQuality = ColQuality + SceQuality + EmotionCorrect)
+  mutate(MemoryQuality = ColQuality + SceQuality + EmotionCorrect) -> behav_df
 
 # the fmri behavioral data, only Ss included in elife paper
-GoodSs <- SS_exclusions.df %>% filter(Memory) %>% pull(SubID)
-behav.df %<>% 
-  filter(SubID %in% GoodSs)
+SS_exclusions_df %>% 
+  filter(Memory) %>% 
+  pull(SubID) -> GoodSs
 
-# Next, we are going to tidy the betas.df to have a session column
-betas.df %<>%
+behav_df %>% 
+  filter(SubID %in% GoodSs) -> behav_df
+
+# Next, we are going to tidy the betas_df to have a session column
+betas_df %>%
   mutate(sess = str_extract(sess, '(?<=Sess)0[1-6]'),
          sess = as.integer(sess)) %>%
-  mutate(ons = round(ons, 3))
+  mutate(ons = round(ons, 3)) -> betas_df
 
 # match the behavioral data to the betas data
-betas.df <- left_join(betas.df, behav.df, 
+betas_df <- left_join(betas_df, behav_df, 
                       by = c('subject' = 'SubID', 
                              'sess' = 'Run', 
                              'ons' = 'onsetRemember'))
 
 # select only the variables of interest; make SceQuality and ColQuality binary; coerce
-# subject to be numeric
-betas.df <- betas.df %>%
-            select(subject, pHipp:pAG, SceQuality, ColQuality, EmotionCorrect) %>%
-	    mutate(ColQuality = as.double(ColQuality > 0), SceQuality = as.double(SceQuality > 0)) %>%
-	    mutate(subject = as.factor(subject)) %>%
-            mutate(subject = as.double(subject))
+# subject to be numeric.
+betas_df %>%
+  select(subject, pHipp:pAG, SceQuality, ColQuality, EmotionCorrect) %>%
+  mutate(ColQuality = as.double(ColQuality > 0), SceQuality = as.double(SceQuality > 0)) %>%
+  mutate(subject = as.factor(subject)) %>%
+  mutate(subject = as.double(subject)) -> betas_df
 
 # write -------------------------------------------------------------------
 
 cat('Writing:\n')
-write_delim(betas.df, 'tidy_roi_data.dat', col_names = F)
+write_delim(betas_df, 'tidy_roi_data.dat', col_names = F)
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Additive_Redundant_Manuscript
-Repository Home of the MemoLab's Additive Redundant Manuscript
+Repository Home of the MemoLab's Additive Redundant Manuscript.
+
+The repository follows the [tidyverse's style guide](https://style.tidyverse.org/index.html).
+
+## Directories
+
+`intermediate/` = contains data written in between analysis steps  
+`mplus/`= mplus specific files  
 
 ## Analysis Steps
 
@@ -7,15 +14,15 @@ Step 1: Extract ROI Data
 - Extract single trial estimates (SPM_T values from a multi-model single trial estimate analysis, see Mumford et al. 2012; [Maureen Ritchey's Generate SPM Single Trial](https://github.com/ritcheym/fmri_misc/blob/master/generate_spm_singletrial.m)) from PM Network ROIs (see Cooper, Kurkela, Davis, & Ritchey 2021; [Publically Available ROIs](https://github.com/memobc/paper-camcan-pmn/tree/master/rois))  
 - Assumes ROIs are stored in a local directory: `rois/`  
 - Assumes single-trial estimates are stored in a local directory: `st_estimates/`  
-- Assumes [SPM12](https://www.fil.ion.ucl.ac.uk/spm/) is located in `spm12/`
-- See: `Extract_ROI_data.m`  
-- See: `Extracted_ROI_data.csv`  
+- Assumes [SPM12](https://www.fil.ion.ucl.ac.uk/spm/) is located in `spm12/`  
+- See: `01_Extract_ROI_data.m`  
+- See: `intermediate/01_Extracted_ROI_data.csv`  
 
 Step 2: Tidy Data
 - Take Extracted Single Trial Estimates and appends behavioral data 'tidying' the data along the way  
 - Assumes behavioral data are stored in a local directory: `orbit-data/`  
-- See: `tidy.R`  
-- See: `mplus/tidy_roi_data.dat`
+- See: `02_tidy.R`  
+- See: `mplus/tidy_roi_data.dat`  
 
 Step 3: Visualize
 - Take tidy data and print a correlation matrix of variables of interest  
@@ -25,9 +32,8 @@ Step 4: MPLUS Modeling
 - Run a series of SEM models in MPLUS. See README in `mplus/`  
 
 
-# Refrences
+# References
 
 Mumford, J. A., Turner, B. O., Ashby, F. G., & Poldrack, R. A. (2012). Deconvolving BOLD activation in event-related designs for multivoxel pattern classification analyses. NeuroImage, 59(3), 2636–2643. https://doi.org/10.1016/j.neuroimage.2011.08.076.
 
 Cooper, R. A., Kurkela, K. A., Davis, S. W., & Ritchey, M. (2021). Mapping the organization and dynamics of the posterior medial network during movie watching. NeuroImage, 236, 118075. https://doi.org/10.1016/j.neuroimage.2021.118075
-
