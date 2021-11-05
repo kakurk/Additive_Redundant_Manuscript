2316f4429bb75028d8f9b5cbae601c8a61aaec39
Kyle Kurkela <kurkela@bc.edu>
2021-11-05 12:06:28 -0400

    Added run models script. Updated mplus models

---
+++ 04_runMplusModels.R
@@ -0,0 +1,13 @@
+# requirements
+library(MplusAutomation)
+
+# parameters
+dataName  <- 'tidy_roi_data_old.dat'
+root.dir  <- '/gsfs0/data/kurkela/Desktop/Additive_Redundant_Manuscript/'
+data.file <- file.path(root.dir, 'intermediate', dataName)
+mplus.dir <- file.path(root.dir, 'mplus')
+mplus.data.file <- file.path(mplus.dir, 'tidy_roi_data.dat')
+
+file.copy(from = data.file, to = mplus.data.file, overwrite = T)
+
+runModels(target = mplus.dir, showOutput = TRUE, logFile = NULL)
